77e095a44c88075b4f301ba6343be32938b809ab
Domantas <92823413+DomantasBal@users.noreply.github.com>
2024-10-08 22:26:44 +0300

    Things to remind things to check

---
+++ dataTypes.js
@@ -0,0 +1,14 @@
+// ### Data Types ###
+// Primitive types - Stored in "stack"
+// String | Number | Boolean | bigInt | Symbol | Null | Undefined
+
+// Reference types - Stored in "heap" accessed by reference
+// Arrays | Functions | Objects
+
+// Things to check:
+// • STACK VS HEAP
+// • LIFO
+// • Stack overflow
+// • Dynamic Memory Alocation
+// • Variables in the stack contain a reference (pointer)
+// • Garbage Collection (Heap)
