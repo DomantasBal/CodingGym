5d009e5988ef210d88bbb69ef6252c7209f2e564
Domantas <92823413+DomantasBal@users.noreply.github.com>
2024-10-08 22:53:26 +0300

    type casting

---
+++ Notes.js
@@ -0,0 +1,35 @@
+// ################## Data Types ##################
+
+// Primitive types - Stored in "stack"
+// String | Number | Boolean | bigInt | Symbol | Null | Undefined
+
+// Reference types - Stored in "heap" accessed by reference
+// Arrays | Functions | Objects
+
+// Check:
+// • STACK VS HEAP
+// • LIFO
+// • Stack overflow
+// • Dynamic Memory Alocation
+// • Variables in the stack contain a reference (pointer)
+// • Garbage Collection (Heap)
+// -----------------------------------------------------------
+
+// ################## Type Conversion - Type Casting ##################
+
+// String -> Number
+// • ParseInt();
+// • Number();
+
+// Number -> String
+// • .toString();
+// • String();
+
+// Decimal num <-> Number
+//  • parseInt();
+//  • parseFloat();
+
+// Number -> Boolean
+// • Boolean();
+
+// NaN - is a property of a global window object. Non writtable and typeof is number.
