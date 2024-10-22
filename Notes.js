593e8ebfdafb3f8f3f89954aa4a054a5ad05a16f
Domantas <92823413+DomantasBal@users.noreply.github.com>
2024-10-22 20:09:34 +0300

    reminder on removing adding classes and toggle

--- Notes.js
+++ Notes.js
@@ -431,3 +431,31 @@
 // • .appendChild()
 // • .remove()
 // • .removeChild()
+
+// ################## JS working with Styles ##################
+
+// const text = document.querySelector('p');
+// const itemList = document.querySelector('.item-list');
+// const items = document.querySelectorAll('li');
+
+// function run() {
+//   className
+//     itemList.className = 'dark';
+
+//     classList
+//     console.log(itemList.classList);
+//     text.classList.add('dark');
+//     text.classList.remove('card');
+//     text.classList.toggle('dark');
+//   text.classList.toggle('hidden');
+// }
+
+// document.querySelector('.btn').onclick = run;
+
+// Reminder:
+// • .className
+// • .classList
+// • .classList.add
+// • .classList.remove
+// • .classList.toggle
+// • .classList.replace('current', 'replace')
