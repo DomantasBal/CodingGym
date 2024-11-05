f98b81b6814e46532fb48bb53700f6000c222909
Domantas <92823413+DomantasBal@users.noreply.github.com>
2024-11-05 22:11:32 +0200

    mini quick n dirty crud revise project with mock api to check the network tab for statuses

--- Tutorials/todo/todo.js
+++ Tutorials/todo/todo.js
@@ -8,6 +8,7 @@ const getTodos = () => {
 
 const addTodoToDOM = (todo) => {
   const div = document.createElement('div');
+  div.classList.add('todo');
   div.appendChild(document.createTextNode(todo.title));
   div.setAttribute('data-id', todo.id);
 
@@ -36,9 +37,41 @@ const createTodo = (e) => {
     .then((data) => addTodoToDOM(data));
 };
 
+const toggleCompleted = (e) => {
+  if (e.target.classList.contains('todo')) {
+    e.target.classList.toggle('done');
+    updateTodo(e.target.dataset.id, e.target.classList.contains('done'));
+  }
+};
+
+const updateTodo = (id, isCompleted) => {
+  fetch(`${apiUrl}/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({ isCompleted }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+const deleteTodo = (e) => {
+  if (e.target.classList.contains('todo')) {
+    const id = e.target.dataset.id;
+    fetch(`${apiUrl}/${id}`, {
+      method: 'DELETE',
+    })
+      .then((response) => response.json())
+      .then(() => e.target.remove());
+  }
+};
+
 const init = () => {
   document.addEventListener('DOMContentLoaded', getTodos);
   document.querySelector('#todo-form').addEventListener('submit', createTodo);
+  document
+    .querySelector('#todo-list')
+    .addEventListener('click', toggleCompleted);
+  document.querySelector('#todo-list').addEventListener('dblclick', deleteTodo);
 };
 
 init();
