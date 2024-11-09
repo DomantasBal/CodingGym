5edc29969750e8d4ab248c6f2fe1d111b631a22e
Domantas <92823413+DomantasBal@users.noreply.github.com>
2024-11-09 21:30:44 +0200

    added swiper

--- Tutorials/movieapp/js/script.js
+++ Tutorials/movieapp/js/script.js
@@ -187,6 +187,52 @@ async function fetchAPIData(endpoint) {
   return data;
 }
 
+async function displaySlider() {
+  const { results } = await fetchAPIData('movie/now_playing');
+
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('swiper-slide');
+
+    div.innerHTML = `<a href="movie-details.html?id=${movie.id}">
+              <img src="https://image.tmdb.org/t/p/w500${
+                movie.poster_path
+              }" alt="${movie.title}}" />
+            </a>
+            <h4 class="swiper-rating">
+              <i class="fas fa-star text-secondary"></i> ${movie.vote_average.toFixed(
+                1
+              )} / 10
+            </h4>`;
+    document.querySelector('.swiper-wrapper').appendChild(div);
+  });
+  initSwiper();
+}
+
+function initSwiper() {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: {
+        slidesPerView: 2,
+      },
+      700: {
+        slidesPerView: 3,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+    },
+  });
+}
+
 // Display backdrop
 function displayBackgroundImage(type, backgroundPath) {
   const overlayDiv = document.createElement('div');
@@ -238,8 +284,8 @@ function init() {
   switch (global.currentPage) {
     case '/':
     case 'index.html':
-      console.log('home');
       displayPopularMovies();
+      displaySlider();
       break;
 
     case '/shows.html':
