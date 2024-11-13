04b9ae2382c416bab8a9c63de8321a66be92376f
Domantas <92823413+DomantasBal@users.noreply.github.com>
2024-11-13 19:21:05 +0200

    experimenting with canvas

---
+++ Tutorials/canvasApi/canvas.js
@@ -0,0 +1,26 @@
+const canvas = document.getElementById('my-canvas');
+const ctx = canvas.getContext('2d'); // ctx - context
+
+// draw rectangle
+ctx.fillStyle = 'green';
+ctx.fillRect(10, 10, 150, 100);
+
+// draw cricle
+ctx.fillStyle = 'red';
+ctx.arc(250, 250, 100, 0, Math.PI * 2);
+ctx.fill();
+
+// draw line
+ctx.beginPath();
+ctx.strokeStyle = 'blue';
+ctx.lineWidth = 10;
+ctx.moveTo(10, 10);
+ctx.lineTo(300, 300);
+ctx.stroke();
+
+// draw text
+ctx.font = '30px Arial';
+ctx.fillStyle = 'orange';
+ctx.lineWidth = 1;
+ctx.fillText('HELLO', 200, 100, 500);
+ctx.strokeText('HELLO', 300, 300, 500);
