5f0a34f309e4c58624cf3c14f00dfdfec081c087
Domantas <92823413+DomantasBal@users.noreply.github.com>
2024-11-20 19:58:34 +0200

    reminer on using classes

--- Notes.js
+++ Notes.js
@@ -1045,7 +1045,7 @@
 
 // console.log(rect.area());
 
-// // ################## Prototypical Inheritance ##################
+// ################## Prototypical Inheritance ##################
 // function Shape(name) {
 //   this.name = name;
 // }
@@ -1080,3 +1080,33 @@
 // console.log(circle);
 
 // circle.logName();
+
+// ################## Classes ##################
+
+class Rectangle {
+  constructor(name, width, height) {
+    this.name = name;
+    this.width = width;
+    this.height = height;
+  }
+
+  area() {
+    return this.height * this.width;
+  }
+
+  perimeter() {
+    return 2 * (this.width + this.height);
+  }
+
+  isSquare() {
+    return this.width === this.height;
+  }
+
+  logArea() {
+    console.log(`Rectangle Area: ${this.area()}`);
+  }
+}
+
+const square = new Rectangle('Square', 20, 20);
+
+square.logArea();
