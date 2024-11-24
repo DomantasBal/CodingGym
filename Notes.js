cd2bfa60acbaa72385df263f0133ff8b5b00216b
Domantas <92823413+DomantasBal@users.noreply.github.com>
2024-11-24 18:51:19 +0200

    exploring defineProperty in constructor functions setters getters

--- Notes.js
+++ Notes.js
@@ -1211,3 +1211,36 @@
 
 // person1.firstName = 'dom';
 // console.log(person1.firstName);
+
+// in Constructor Functions
+function Person(firstName, lastName) {
+  this._firstName = firstName;
+  this._lastName = lastName;
+
+  Object.defineProperty(this, 'firstName', {
+    get: function () {
+      return this.capitalizeFirst(this._firstName);
+    },
+    set: function () {
+      this._firstName;
+    },
+  });
+
+  Object.defineProperty(this, 'lastName', {
+    get: function () {
+      return this.capitalizeFirst(this._lastName);
+    },
+    set: function () {
+      this._lastName;
+    },
+  });
+}
+
+Person.prototype.capitalizeFirst = function (value) {
+  return value.charAt(0).toUpperCase() + value.slice(1);
+};
+
+const person1 = new Person('Dom', 'bal');
+
+console.log(person1.firstName);
+console.log(person1.lastName);
