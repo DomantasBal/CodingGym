72aef46664cae4e458393ac8403f4569175850c4
Domantas <92823413+DomantasBal@users.noreply.github.com>
2024-12-05 16:14:05 +0200

    added calorie limit update

--- MiniProjects/tracalorie_theme/js/app.js
+++ MiniProjects/tracalorie_theme/js/app.js
@@ -14,6 +14,12 @@ class CalorieTracker {
     this._displayCaloriesProgress();
   }
 
+  setLimit(calorieLimit) {
+    this._calorieLimit = calorieLimit;
+    this._displayCaloriesLimit();
+    this._render();
+  }
+
   // Public Methods
   addMeal(meal) {
     this._meals.push(meal);
@@ -233,6 +239,10 @@ class App {
     document
       .getElementById('reset')
       .addEventListener('click', this._reset.bind(this));
+
+    document
+      .getElementById('limit-form')
+      .addEventListener('submit', this._setLimit.bind(this));
   }
 
   _newItem(type, e) {
@@ -302,6 +312,24 @@ class App {
     document.getElementById('workout-items').innerHTML = '';
     document.getElementById('filter-meals').value = '';
     document.getElementById('filter-workouts').value = '';
+  }
+
+  _setLimit(e) {
+    e.preventDefault();
+
+    const limit = document.getElementById('limit');
+
+    if (limit.value === '') {
+      alert('Please add a limit');
+      return;
+    }
+
+    this._tracker.setLimit(+limit.value); //+ converts to num
+    limit.value = '';
+
+    const modalEl = document.getElementById('limit-modal');
+    const modal = bootstrap.Modal.getInstance(modalEl);
+    modal.hide();
   }
 }
 const app = new App();
