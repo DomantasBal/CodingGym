adc0be218eb51b63c878326ea1e3646d4406887d
Domantas <92823413+DomantasBal@users.noreply.github.com>
2024-12-10 18:22:24 +0200

    reminder on webpack config

---
+++ Webpack/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'), // path using for output is dist folder
+    filename: 'bundle.js',
+  },
+};
