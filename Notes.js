d201eeb7626fdb0f40a697deaa828d1b062b0a7f
Domantas <92823413+DomantasBal@users.noreply.github.com>
2025-01-01 16:23:23 +0200

    trying linked lists

--- Notes.js
+++ Notes.js
@@ -1617,52 +1617,213 @@
 // console.log('Top item: ', stack.peek());
 
 // ################### Queues (FIFO) ################
-class Queue {
-  constructor() {
-    this._items = [];
-    this._count = 0;
-    this._front = 0;
-  }
+// class Queue {
+//   constructor() {
+//     this._items = [];
+//     this._count = 0;
+//     this._front = 0;
+//   }
 
-  enqueue(item) {
-    this._items[this._count] = item;
-    this._count++;
-  }
+//   enqueue(item) {
+//     this._items[this._count] = item;
+//     this._count++;
+//   }
 
-  dequeue() {
-    if (this.isEmpty()) {
-      return undefined;
-    }
+//   dequeue() {
+//     if (this.isEmpty()) {
+//       return undefined;
+//     }
 
-    const item = this._items[this._front];
-    for (let i = this._front; i < this._count - 1; i++) {
-      this._items[i] = this._items[i + 1];
-    }
-    this._count--;
-    this._items.length = this._count;
-    return item;
-  }
+//     const item = this._items[this._front];
+//     for (let i = this._front; i < this._count - 1; i++) {
+//       this._items[i] = this._items[i + 1];
+//     }
+//     this._count--;
+//     this._items.length = this._count;
+//     return item;
+//   }
 
-  peek() {
-    if (this.isEmpty()) {
-      return 'No items in the queue';
-    }
-    return this._items[this._front];
-  }
+//   peek() {
+//     if (this.isEmpty()) {
+//       return 'No items in the queue';
+//     }
+//     return this._items[this._front];
+//   }
 
-  length() {
-    return this._count - this._front;
-  }
+//   length() {
+//     return this._count - this._front;
+//   }
 
-  isEmpty() {
-    return this._count === 0;
-  }
-}
+//   isEmpty() {
+//     return this._count === 0;
+//   }
+// }
 
-const q = new Queue();
-q.enqueue('Item 1');
-q.enqueue('Item 2');
-q.enqueue('Item 3');
+// const q = new Queue();
+// q.enqueue('Item 1');
+// q.enqueue('Item 2');
+// q.enqueue('Item 3');
 
-console.log('Front item:', q.peek());
-console.log('Queue length:', q.length());
+// console.log('Front item:', q.peek());
+// console.log('Queue length:', q.length());
+
+// ################### Linked Lists ################
+// • HEAD
+// • TAIL
+
+// Hardcoded linked list
+// const node1 = {
+//   value: 100,
+// };
+
+// const node2 = {
+//   value: 200,
+// };
+
+// const node3 = {
+//   value: 300,
+// };
+
+// node1.next = node2;
+// node2.next = node3;
+// node3.next = null;
+// console.log(node1, node2, node3);
+
+// Implementation of linked list
+// class Node {
+//   constructor(value) {
+//     this._value = value;
+//     this.next = null;
+//   }
+// }
+
+// class LinkedList {
+//   constructor() {
+//     this._head = null;
+//     this.length = 0;
+//   }
+
+//   // Insert first node (HEAD)
+//   insertFirst(value) {
+//     const newNode = new Node(value);
+//     newNode.next = this._head;
+//     this._head = newNode;
+//     this.length++;
+//   }
+
+//   // Insert last node (TAIL)
+//   insertLast(value) {
+//     const newNode = new Node(value);
+//     let current = this._head;
+//     while (current.next) {
+//       current = current.next;
+//     }
+
+//     current.next = newNode;
+//     this._length++;
+//   }
+
+//   // Insert at Index
+//   insertAt(value, index) {
+//     if (index > this._length) {
+//       return;
+//     }
+
+//     if (index === 0) {
+//       this.insertFirst(value);
+//       return;
+//     }
+
+//     const newNode = new Node(value);
+//     let current, previous;
+//     current = this._head;
+//     let count = 0;
+
+//     while (count < index) {
+//       previous = current;
+//       current = current.next;
+//       count++;
+//     }
+
+//     newNode.next = current;
+//     previous.next = newNode;
+//     this._length++;
+//   }
+
+//   // Get at Index
+//   getAt(index) {
+//     let current = this._head;
+//     let count = 0;
+
+//     while (current) {
+//       if (count === index) {
+//         console.log(current._value);
+//       }
+//       count++;
+//       current = current.next;
+//     }
+//     return null;
+//   }
+
+//   // Remove at index
+//   removeAt(index) {
+//     if (index > this._length) {
+//       return;
+//     }
+
+//     let current = this._head;
+//     let previous;
+//     let count = 0;
+
+//     if (index === 0) {
+//       this._head = current.next;
+//     } else {
+//       while (count < index) {
+//         count++;
+//         previous = current;
+//         current = current.next;
+//       }
+//       previous.next = current.next;
+//     }
+
+//     this._length--;
+//   }
+
+//   // Print list data
+//   printListData() {
+//     let current = this._head;
+//     let list = '';
+
+//     while (current) {
+//       list += current._value + ' ';
+//       current = current.next;
+//     }
+
+//     console.log(list);
+//   }
+
+//   // Clear list
+//   clearListData() {
+//     this._head = null;
+//     this._length = 0;
+//   }
+// }
+
+// const list = new LinkedList();
+// list.insertFirst(100);
+// list.insertFirst(200);
+// list.insertFirst(300);
+
+// list.insertLast(50);
+// list.insertLast(10);
+
+// list.insertAt(500, 2);
+// list.insertAt(600, 4);
+
+// list.removeAt(2);
+
+// // list.clearListData();
+
+// list.printListData();
+// list.getAt(2);
+// list.getAt(3);
