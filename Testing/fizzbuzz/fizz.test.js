0efe006730e2d5c3d75774e8385f060aca9253e2
Domantas <92823413+DomantasBal@users.noreply.github.com>
2025-01-02 21:11:17 +0200

    reminder on jest

--- Testing/fizzbuzz/fizz.test.js
+++ Testing/fizzbuzz/fizz.test.js
@@ -8,4 +8,16 @@ describe('fizzbuzz', () => {
   it('should return a number if not divisible by 3 or 5', () => {
     expect(fizzBuzz(1)).toEqual(1);
   });
+
+  it('should return fizz if divisable by 3', () => {
+    expect(fizzBuzz(3)).toEqual('Fizz');
+    expect(fizzBuzz(6)).toEqual('Fizz');
+    expect(fizzBuzz(12)).toEqual('Fizz');
+  });
+
+  it('should return fizz if divisable by 5', () => {
+    expect(fizzBuzz(5)).toEqual('Buzz');
+    expect(fizzBuzz(10)).toEqual('Buzz');
+    expect(fizzBuzz(20)).toEqual('Buzz');
+  });
 });
