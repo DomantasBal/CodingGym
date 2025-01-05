791090af3f3f5deae64025e9aa268099f3d3a408
Domantas <92823413+DomantasBal@users.noreply.github.com>
2025-01-05 20:01:57 +0200

    reminder on fs

--- Node.js/fs.js
+++ Node.js/fs.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-// const fs = require('fs/promises');
+// const fs = require('fs');
+const fs = require('fs/promises');
 
 // Write to a file
 
@@ -16,5 +16,63 @@ const fs = require('fs');
 //   .catch((err) => console.log(err));
 
 // Sync version
-fs.writeFileSync('file3.txt', 'Helllllllooooo ');
-console.log('file created');
+// fs.writeFileSync('file3.txt', 'Helllllllooooo ');
+// console.log('file created');
+
+// Async / Await
+// async function createFile(filename, content) {
+//   try {
+//     await fs.writeFile(filename, content);
+//     console.log('file created');
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+// createFile('text1.txt', 'test test 123');
+
+// Read from file
+// async function readFile(filename) {
+//   try {
+//     const data = await fs.readFile(filename, 'utf8');
+//     console.log(data);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+// readFile('file1.txt');
+
+// Delete file
+// async function deleteFile(filename) {
+//   try {
+//     await fs.unlink(filename);
+//     console.log(`${filename} is deleted`);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+// deleteFile('file2.txt');
+
+// Rename file
+// async function renameFile(oldName, newName) {
+//   try {
+//     await fs.rename(oldName, newName);
+//     console.log(`file with old name: ${oldName} renamed to ${newName}`);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+// renameFile('file3.txt', 'renamed.txt');
+
+// Create Folder
+// async function createFolder(folderName) {
+//   try {
+//     await fs.mkdir(folderName);
+//     console.log(`Folder ${folderName} created`);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+// createFolder('folder1');
