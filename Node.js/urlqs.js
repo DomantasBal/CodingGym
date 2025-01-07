0d9ed61b613eee8c1b43cfa8918a40443e7ff5ed
Domantas <92823413+DomantasBal@users.noreply.github.com>
2025-01-07 23:25:29 +0200

    url mothods

---
+++ Node.js/urlqs.js
@@ -0,0 +1,34 @@
+const url = require('url');
+const querystring = require('querystring');
+
+// // url.parse()
+// const myUrl = url.parse('https://example.com/listing?id=1000&premium=true');
+// console.log(myUrl);
+
+// // url.format()
+// const myUrl2 = url.format({
+//   protocol: 'https',
+//   host: 'www.example.com',
+//   path: 'listing',
+//   query: {
+//     id: 1000,
+//     premium: true,
+//   },
+// });
+
+// console.log(myUrl2);
+
+// // querystring.parse()
+// const myqs = 'year=2025&month=january&day=20';
+// const q = querystring.parse(myqs);
+// console.log(q);
+// console.log(q.month);
+
+// // querystring.strigify()
+// const myQs2 = querystring.stringify({
+//   year: 2025,
+//   month: 'January',
+//   day: 20,
+// });
+
+// console.log(myQs2);
