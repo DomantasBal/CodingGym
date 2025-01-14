c83fef0283f170e0394a7ec994bcae17ab56ae3d
Domantas <92823413+DomantasBal@users.noreply.github.com>
2025-01-14 21:04:24 +0200

    refactor delete

--- MiniProjects/randomIdeas-API/routes/ideas.js
+++ MiniProjects/randomIdeas-API/routes/ideas.js
@@ -83,17 +83,14 @@ router.put('/:id', async (req, res) => {
 });
 
 // Delete
-router.delete('/:id', (req, res) => {
-  const idea = ideas.find((idea) => idea.id === +req.params.id);
-
-  if (!idea) {
-    return res.status(404).json({ success: false, error: 'Not deleted' });
+router.delete('/:id', async (req, res) => {
+  try {
+    await Idea.findByIdAndDelete(req.params.id);
+    res.json({ success: true, data: {} });
+  } catch (err) {
+    res.status(500).json({ success: false, err: 'something went wrong' });
+    console.log(err);
   }
-
-  const index = ideas.indexOf(idea);
-  ideas.splice(index, 1);
-
-  res.json({ success: true, data: {} });
 });
 
 module.exports = router;
